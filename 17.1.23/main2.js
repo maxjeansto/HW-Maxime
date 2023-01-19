f298049e3de162af92db064c3d5a18a5e014cd31
Maxime Saada <maximesaada@MacBook-Pro-de-Maxime.local>
2023-01-19 11:14:45 +0200

    17.1.23

--- 17.1.23/main2.js
+++ 17.1.23/main2.js
@@ -1,20 +1,26 @@
-function letters(str) {
-    var word = {}
-    var letter = str.split("");
-    for (let i = 0; i < letter.length; i++) {
-        if (word[letter[i]]) {
-            word[letter[i]]++
+function separateLetter(str) {
+    var result = [];
+    var letters = str.split('');
+
+    for (let index = 0; index < letters.length; index++) {
+        var letter = letters[index];
+        var equal = false;
+        for (let index2 = 0; index2 < result.length; index2++) {
+            if (result[index2].lettre === letter) {
+                equal = true;
+                result[index2].count++
+                break;
+            }
         }
-        else {
-        word[letter[i]] = 1;
+        if (equal === false) {
+            result.push({ lettre: letter, count: 1 });
+
+        }
     }
 
-
-    }
-    //console.log(word);
-return word;
+return result;
 }
 
 var string = prompt("inserez votre mot").toLowerCase();
-var result = letters(string);
-console.log(result);
+var results = separateLetter(string);
+console.log(results);
